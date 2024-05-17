c9b2077266bcf899da854393b123b559ab6a6e6c
nizamudheenknr <nizamudheenknr@gmail.com>
2024-05-17 15:00:22 +0530

    Create User Schema

---
+++ Index.js
@@ -0,0 +1,15 @@
+const express = require("express")
+
+
+const app = express();
+const port = 3030;
+
+
+app.get('/',(req,res)=>{
+    res.send("hello,world")
+})
+
+
+app.listen(port,()=>{
+    console.log(`listening server ${port}`);
+})
